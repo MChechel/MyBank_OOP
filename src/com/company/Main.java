0f01e42e733d63801d1e9160b019f58da74ef33d
MIKE <mixa262@gmail.com>
2021-11-06 12:40:01 +0200

    The task is stated here

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,19 @@ public class Main {
         //06.11.2021
         /*
         * TASK 4 - PROJECT
+        * write class called BankAccount
+        * it must have:
+        * - name of user
+        * - account number
+        * - amount in the account
+        * - name of Bank
+        * - pin code(account password)
+        *
+        * methods:
+        * depositMoney
+        * transferMoney
+        * withdrawMoney
+        *
         * */
     }
 }
