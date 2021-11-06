4d0f288de5cc74ab4c0df08c964a2cff03e68327
MIKE <mixa262@gmail.com>
2021-11-06 12:34:40 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //06.11.2021
+        /*
+        * TASK 4 - PROJECT
+        * */
+    }
+}
